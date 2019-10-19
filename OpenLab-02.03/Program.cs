bba0af0b85d911e5896817b64ec9713dc7f02763
simonheglasik <simonheglasik@gmail.com>
2019-10-19 17:55:29 +0200

    02.03

---
+++ OpenLab-02.03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OpenLab_02._03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                string a = Console.ReadLine();
+                string b = Console.ReadLine();
+                int aa = int.Parse(a);
+                int bb = int.Parse(b);
+                int remainder = aa % bb;
+                Console.WriteLine($"Remainder is {remainder}");
+            }
+
+        }
+    }
+}
