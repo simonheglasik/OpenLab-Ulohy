fe47073775626555d68710fc18705604b70604ee
simonheglasik <simonheglasik@gmail.com>
2019-10-19 17:57:35 +0200

    02.04

---
+++ OpenLab-02.04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OpenLab_Uloha_02._04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chicken = 1;
+            int cow = 1;
+            int pig = 1;
+
+            int sum = s(chicken, cow, pig);
+            Console.WriteLine(sum);
+        }
+        public static int s(int a, int b, int c)
+        {
+            return (a * 2) + (b * 4) + (c * 4);
+        }
+    }
+}
