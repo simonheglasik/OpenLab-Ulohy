ff6813208419ddd8ee315923970309ba2103856a
simonheglasik <simonheglasik@gmail.com>
2019-10-21 17:29:28 +0200

    02.07

---
+++ OpenLab-02.07/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OpenLab_02._07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          bool a = true;
+          a = DateTime.Now == new DateTime(2019, 12, 24);
+          Console.WriteLine(a);
+        }
+
+    }
+}
