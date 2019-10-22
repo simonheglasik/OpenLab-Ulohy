e5e6ebcd5602eb1abce1e99962233ee1373194ae
simonheglasik <simonheglasik@gmail.com>
2019-10-22 13:36:52 +0200

    02.08_Object

---
+++ OpenLab-02-08_Object/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IsEmpty
+{
+    class Empty
+    {
+        public void Ep(string a)
+        {
+            bool b = true;
+            b = a == "";
+            Console.WriteLine(b);
+        }
+    }
+}
---
+++ OpenLab-02.08/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using IsEmpty;
+
+namespace OpenLab_02._08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Empty empty;
+                empty = new Empty();
+                string a = Console.ReadLine();
+                empty.Ep(a);
+            }
+
+        }
+    }
+}
