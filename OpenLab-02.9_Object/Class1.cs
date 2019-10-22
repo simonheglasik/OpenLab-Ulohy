61b84daea86ab56159c1c8e9f3ad0d599a75fab1
simonheglasik <simonheglasik@gmail.com>
2019-10-22 15:10:14 +0200

    02.09_Object

---
+++ OpenLab-02.9_Object/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nam
+{
+    class Names
+    {
+        public void Name(string a,string b)
+        {
+            Console.WriteLine($"{a} {b}");
+        }
+    }
+}
---
+++ OpenLab-02.9_Object/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Nam;
+namespace OpenLab_02._9_Object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = "Simon";
+            string last = "Heglasík";
+            Names name;
+            name = new Names();
+            name.Name(first, last);
+        }
+    }
+}
