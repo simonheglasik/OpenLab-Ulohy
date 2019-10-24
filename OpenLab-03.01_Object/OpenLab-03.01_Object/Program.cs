31874c6acfb0956e6d651cd73266e3f2a8f1fe38
simonheglasik <simonheglasik@gmail.com>
2019-10-24 09:17:21 +0200

    sdf

---
+++ OpenLab-03.01_Object/OpenLab-03.01_Object/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using dva;
+
+namespace OpenLab_03._01_Object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Tabule tabule = new Tabule();
+            tabule.Tab();
+        }
+    }
+}
