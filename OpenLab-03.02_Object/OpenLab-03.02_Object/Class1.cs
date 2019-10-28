c46e712623b5186edf21e2505036dcef6e1de52d
simonheglasik <simonheglasik@gmail.com>
2019-10-28 11:02:55 +0100

    +vyzva

---
+++ OpenLab-03.02_Object/OpenLab-03.02_Object/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenLab_03._02_Object
+{
+    class Class1
+    {
+        public void Tab()
+        {
+            string[,] tab = new string[2, 3] { { "jozef", "Fero", "Karol" }, { "Ahoj", "je", "prec" } };
+            for (int a = 0; a < 2; a++)
+            {
+                for (int b = 0; b < 3; b++)
+                {
+                     Console.Write((tab[a, b]) + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ OpenLab-03.02_Object/OpenLab-03.02_Object/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using OpenLab_03._02_Object;
+namespace OpenLab_03._02_Object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Class1 clas = new Class1();
+            clas.Tab();
+        }
+    }
+}
