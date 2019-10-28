7da307cbbea19f6d1a476a8695cf33871494bae0
Simon Heglasik <38507657+simonheglasik@users.noreply.github.com>
2019-10-28 19:57:23 +0100

    03.03

---
+++ Open-Lab-03.03/Comparator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Open_Lab_03._03
+{
+    public class Comparator
+    {
+        public bool CompareCharactersCount(string str1, string str2)
+        {
+            bool a = true;
+            a = str1.Length == str2.Length;
+            return a;
+        }
+    }
+}
