27e3bc79042db2db887ef78dcd1ad038db23b74d
Simon Heglasik <38507657+simonheglasik@users.noreply.github.com>
2019-10-28 21:13:16 +0100

    03.04

---
+++ Open-Lab-03.04/Numbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Open_Lab_03._04
+{
+    public class Numbers
+    {
+        public string EvenOrOdd(int number)
+        {
+            bool a = number % 2 == 0;
+            if (a =!a)
+            {
+                return "odd";
+            }
+            else
+            {
+                return "even";
+            }
+            
+        }
+    }
+}
