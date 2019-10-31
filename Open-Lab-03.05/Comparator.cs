c0be1c9f85982b38da8c50ba62848b74a6654358
Simon Heglasik <38507657+simonheglasik@users.noreply.github.com>
2019-10-31 16:36:30 +0100

    03.05

---
+++ Open-Lab-03.05/Comparator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Open_Lab_03._05
+{
+    public class Comparator
+    {
+        public bool MatchCaseInsensitive(string str1, string str2)
+        {    
+            return str1.ToLower() == str2.ToLower();
+        }
+    }
+}
